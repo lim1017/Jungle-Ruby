88254f709fecb8c19f1c2e1c3d8747c38ab34dbb
lim1017 <lim1017@hotmail.com>
2020-03-05 17:21:23 +0000

    admin categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,33 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_KEY'], password: ENV['ADMIN_PW']
+
+
+
+  def index
+    @categories = Category.all()
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'category created!'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :category_id
+    )
+  end
+end
